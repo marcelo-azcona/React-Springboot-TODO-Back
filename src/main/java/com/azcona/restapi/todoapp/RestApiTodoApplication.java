b1b1e45930548bf2ee4ce82c68fe47e15e303ecc
marcelo-azcona <marceloazcona86@gmail.com>
2023-04-03 18:05:36 -0300

    Uploading initial files

---
+++ src/main/java/com/azcona/restapi/todoapp/RestApiTodoApplication.java
@@ -0,0 +1,13 @@
+package com.azcona.restapi.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTodoApplication.class, args);
+	}
+
+}
